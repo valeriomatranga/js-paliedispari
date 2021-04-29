f08327aa5b0b581c265ec1b9c850ea5b52fd139a
valeriomatranga <matrangav@gmail.com>
2021-04-29 16:50:20 +0200

    function

---
+++ asset/js/main.js
@@ -0,0 +1,12 @@
+//chiedere una parola all'utente
+/* var parolaUtente = prompt("Scrivi una parola qualsiasi!");
+ */
+//verificare tramite una funzione che la parola inserita sia palindroma
+function palindroma (testo){
+    parola = parola.split
+    console.log(parola);
+    
+}
+
+var parola = prompt("Scrivi una parola qualsiasi!");
+palindroma(parola) 
